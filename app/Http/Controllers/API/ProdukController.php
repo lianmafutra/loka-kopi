29db289f08829dba0382132ae7412dc7d1a23740
lianmafutra <lianmafutra@gmail.com>
2024-09-11 17:38:12 +0700

    komposisi update array

--- app/Http/Controllers/API/ProdukController.php
+++ app/Http/Controllers/API/ProdukController.php
@@ -1,34 +1,37 @@
 <?php
-
 namespace App\Http\Controllers\API;
-
 use App\Http\Controllers\Controller;
 use App\Models\Produk;
 use App\Utils\ApiResponse;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
-
 class ProdukController extends Controller
 {
     use ApiResponse;
-
     public function list(): JsonResponse
     {
       $produk = Produk::get();
       return $this->success("List Produk", $produk);
     }
 
+
     public function detail($produk_id)
     {
-      try {
-         $produk = Produk::findOrFail($produk_id)->makeHidden(['stok','diskon']);
-       
-     } catch (ModelNotFoundException $e) {
-         return $this->error("Produk tidak ditemukan", 404);
-     }
-    
-    
-      return $this->success("Detail Produk", $produk);
+            try {
+               $produk = Produk::findOrFail($produk_id)->makeHidden(['stok','diskon']);
+         } catch (ModelNotFoundException $e) {
+               return $this->error("Produk tidak ditemukan", 404);
+         }
+         $produkTransform = [
+            'id' =>  $produk?->id,
+            'nama' =>  $produk?->nama,
+            'foto' =>  $produk?->foto,
+            'desc_short' =>  $produk?->desc_short,
+            'harga' =>  $produk?->harga,
+            'komposisi' =>  array_filter(array_map('trim', explode(';', $produk?->komposisi))),
+            'promo' =>  $produk?->foto,
+            'foto_url' => $produk?->foto_url,
+         ];
+      return $this->success("Detail Produk", $produkTransform);
     }
-
 }
