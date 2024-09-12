0e4c4df3bcaa41320d2f190aa059af8f9be43bfe
lianmafutra <lianmafutra@gmail.com>
2024-09-12 19:12:50 +0700

    test mobile

--- resources/views/app/transaksi/create.blade.php
+++ resources/views/app/transaksi/create.blade.php
@@ -1,20 +1,38 @@
-@extends('admin.layouts.master')
-@push('css')
-    <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }} ">
-    <link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('plugins/flatpicker/flatpickr.min.css') }}">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Bootstrap Layout</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Select2 CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet">
+</head>
+<style>
+   .card-footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #f8f9fa; /* atau warna latar belakang yang Anda inginkan */
+  padding: 10px;
+  box-shadow: 0 -1px 5px rgba(0, 0, 0, 0.1);
+  z-index: 1000; /* Pastikan footer berada di atas elemen lain */
+}
 
-    <style>
+.main-content {
+  margin-bottom: 90px; /* Sesuaikan dengan tinggi footer */
+}
+</style>
+<body>
 
-
-
-    </style>
-@endpush
-@section('header')
-    <x-header title="Input Data Transaksi" back-button="true"></x-header>
-@endsection
-@section('content')
-    <form id="form_sample" method="post" enctype="multipart/form-data">
+   
+    <!-- Main Content -->
+    <div class="main-content container my-5">
+        <div class="row">
+            <form id="form_sample" method="post" enctype="multipart/form-data">
         <div class="row">
             <div class="col-lg-8 col-sm-12">
                 @csrf
@@ -35,7 +53,7 @@
                             <tbody>
                                 <tr>
                                     <td >
-                                        <select data-placeholder="Pilih Produk"  style="width: 100%" name="products[0][id]" class="product-select" required>
+                                        <select data-placeholder="Pilih Produk"  style="width: 200px" name="products[0][id]" class="product-select" required>
                                             <option value="">Pilih Produk</option>
                                             @foreach ($products as $product)
                                                 <option value="{{ $product->id }}">{{ $product->nama }}</option>
@@ -45,7 +63,13 @@
                                
 
                                     <td><input type="number" name="products[0][quantity]" class="form-control" required /></td>
-                                    <td><button type="button" class="btn btn-danger remove-row remove-tr">Hapus</button></td>
+                                    <td>
+                                       <button type="button" class="btn btn-danger remove-row remove-tr">
+                                          <i class="fas fa-trash-alt"></i>
+                                        </button>
+
+                                    </td>
+                                 
                                 </tr>
                             </tbody>
                         </table>
@@ -54,96 +78,103 @@
                     </div>
 
                     <div class="card-footer">
-                        <div style="gap:8px;" class="d-flex">
-                            <a href="{{ route('transaksi.index') }}" type="button"
-                                class="btn btn-secondary">Kembali</a>
-
-                            <button type="button" class="btn btn-warning" id="add-row">Add Row</button>
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                        </div>
-                    </div>
+                     <div style="gap:8px;" class="d-flex">
+                       {{-- <a href="{{ route('transaksi.index') }}" type="button" class="btn btn-secondary">Kembali</a> --}}
+                       <button type="button" class="btn btn-warning" id="add-row">Tambah</button>
+                       <button type="submit" class="btn btn-primary">Simpan</button>
+                     </div>
+                   </div>
                 </div>
             </div>
 
         </div>
     </form>
-@endsection
-@push('js')
-    <script src="{{ asset('plugins/select2/js/select2.full.min.js') }}"></script>
+        </div>
+    </div>
 
+  
 
+   <!-- jQuery -->
+   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+   <!-- Bootstrap JS and Popper.js -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+   <!-- Select2 JS -->
+   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script>
-        $(function() {
+      $(function() {
 
 
 
 
-          
+        
 
-            $('#form_sample').submit(function(e) {
-                e.preventDefault();
-                const formData = new FormData(this);
-                $.ajax({
-                    type: 'POST',
-                    url: route('transaksi.store'),
-                    data: formData,
-                    contentType: false,
-                    processData: false,
-                    dataType: 'json',
-                    beforeSend: function() {
-                        _showLoading()
-                    },
-                    success: (response) => {
-                        if (response) {
-                            Swal.fire({
-                                icon: 'success',
-                                title: response.message,
-                                showCancelButton: true,
-                                allowEscapeKey: false,
-                                showCancelButton: false,
-                                allowOutsideClick: false,
-                            }).then((result) => {
-                                window.location.replace(route(
-                                    'transaksi.index'))
-                            })
-                        }
-                    },
-                    error: function(response) {
-                        _showError(response)
-                    }
-                })
-            })
-
-
-
-            $('.product-select').select2({
-               theme: 'bootstrap4',
-            });
-     
-            $('#add-row').click(function() {
-                var rowCount = $('#products-table tbody tr').length;
-                var newRow = `
-        <tr>
-            <td>
-                <select data-placeholder="Pilih Produk" style="width: 100%" name="products[${rowCount}][id]" class="product-select" required>
-                    <option value="">Pilih Produk</option>
-                    @foreach ($products as $product)
-                        <option value="{{ $product->id }}">{{ $product->nama }}</option>
-                    @endforeach
-                </select>
-            </td>
-            <td><input type="number"  class="form-control" name="products[${rowCount}][quantity]" required></td>
+          $('#form_sample').submit(function(e) {
+              e.preventDefault();
+              const formData = new FormData(this);
+              $.ajax({
+                  type: 'POST',
+                  url: route('transaksi.store'),
+                  data: formData,
+                  contentType: false,
+                  processData: false,
+                  dataType: 'json',
+                  beforeSend: function() {
+                      _showLoading()
+                  },
+                  success: (response) => {
+                      if (response) {
+                          Swal.fire({
+                              icon: 'success',
+                              title: response.message,
+                              showCancelButton: true,
+                              allowEscapeKey: false,
+                              showCancelButton: false,
+                              allowOutsideClick: false,
+                          }).then((result) => {
+                              window.location.replace(route(
+                                  'transaksi.index'))
+                          })
+                      }
+                  },
+                  error: function(response) {
+                      _showError(response)
+                  }
+              })
+          })
 
 
-            <td><button type="button" class="btn btn-danger remove-row">Hapus</button></td>
-        </tr>`;
-                $('#products-table tbody').append(newRow);
-                $('.product-select').select2(); // Reinitialize select2 for new rows
-            });
 
-            $(document).on('click', '.remove-row', function() {
-                $(this).closest('tr').remove();
-            });
-        })
-    </script>
-@endpush
+          $('.product-select').select2({
+            //  theme: 'bootstrap4',
+          });
+   
+          $('#add-row').click(function() {
+              var rowCount = $('#products-table tbody tr').length;
+              var newRow = `
+      <tr>
+          <td>
+              <select data-placeholder="Pilih Produk" style="width: 200px"  name="products[${rowCount}][id]" class="product-select" required>
+                  <option value="">Pilih Produk</option>
+                  @foreach ($products as $product)
+                      <option value="{{ $product->id }}">{{ $product->nama }}</option>
+                  @endforeach
+              </select>
+          </td>
+          <td><input type="number"  class="form-control" name="products[${rowCount}][quantity]" required></td>
+
+
+          <td>  <button type="button" class="btn btn-danger remove-row remove-tr">
+                                          <i class="fas fa-trash-alt"></i>
+                                        </button></td>
+      </tr>`;
+              $('#products-table tbody').append(newRow);
+              $('.product-select').select2(); // Reinitialize select2 for new rows
+          });
+
+          $(document).on('click', '.remove-row', function() {
+              $(this).closest('tr').remove();
+          });
+      })
+  </script>
+</body>
+</html>
