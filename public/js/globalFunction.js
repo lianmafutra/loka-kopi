04976df76f13fabc49e3a4ed4b3a4dd5edbf594d
lianmafutra <lianmafutra@gmail.com>
2024-09-12 12:06:23 +0700

    input multiple transaksi

--- public/js/globalFunction.js
+++ public/js/globalFunction.js
@@ -210,7 +210,6 @@ $('.select2-search__field').css('width', '100%');
 $(document).on('select2:open', function (e) {
    if(e.target.id != ""){
       document.querySelector('[aria-controls="select2-' + e.target.id + '-results"]').focus();
-     
    }
  
 })
